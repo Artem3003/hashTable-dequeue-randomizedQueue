7b581eb3009b677b28406ecb6554da05a4eb0876
Artem <temapro04@gmai.com>
2023-07-21 21:44:55 +0300

    Second task of hash table with unit tests

--- Program.cs
+++ Program.cs
@@ -1,12 +1,161 @@
 ﻿using System;
+using NUnit.Framework;
 
 namespace HashTableDequeRandomizedQueue
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            // Deque
+            UnitTests tests = new UnitTests();
+            tests.TestDequeAddFirst();
+            tests.TestDequeAddLast();
+            tests.TestDequeIsEmpty();
+            tests.TestDequeIterator();
+            tests.TestDequeRemoveFirst();
+            tests.TestDequeRemoveLast();
+            tests.TestDequeSize();
+
+            //Randomized queue
+            tests.TestRandomizedQueueDequeue();
+            tests.TestRandomizedQueueEnqueue();
+            tests.TestRandomizedQueueIsEmpty();
+            tests.TestRandomizedQueueIterator();
+            tests.TestRandomizedQueueSample();
+            tests.TestRandomizedQueueSize();
+
+            // Hash Table
+            HashTable<string, bool> dictionary = new HashTable<string, bool>(50);
+
+            using (StreamReader reader = new StreamReader("./numbers.txt"))
+            {
+                string number;
+                while ((number = reader.ReadLine()) != null)
+                {
+                    dictionary.Add(number, true);
+                }
+                
+            }
+
+           Console.WriteLine(dictionary.Size());
             
+            Console.WriteLine(Ispell("Forty-seven", dictionary));
+            Console.WriteLine(Ispell("Hundret", dictionary));
+
+            Console.WriteLine("==============");
+
+            AddTest();
+            RemoveTest();
+            GetTest();
+            ContainsTest();
+            ClearTest();
+            SizeTest();
+        }
+
+        // second part of the assignment
+        public static bool Ispell(string word, HashTable<string, bool> dictionary)
+        {
+            try
+            {
+                if (dictionary.Get(word))
+                {
+                    Console.WriteLine("OK");
+                    return true;
+                }
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("WrongSpelling");
+                return false;
+            }
+            return false;
+        }
+
+        // unit tests
+        public static void AddTest()
+        {
+            HashTable<int, string> hashTable = new HashTable<int, string>();
+
+            hashTable.Add(1, "apple");
+            hashTable.Add(2, "banana");
+
+            string apple = hashTable.Get(1);
+            string banana = hashTable.Get(2);
+
+            Assert.AreEqual("apple", apple);
+            Assert.AreEqual("banana", banana);
+            Console.WriteLine("PASSED: AddTest");
+        }   
+
+        public static void RemoveTest()
+        {
+            HashTable<int, string> hashTable = new HashTable<int, string>();
+            
+            hashTable.Add(1, "apple");
+            hashTable.Add(2, "banana");
+
+            Assert.IsTrue(hashTable.Contains("apple"));
+
+            hashTable.Remove(1); // remove banana
+
+            Assert.IsFalse(hashTable.Contains("apple"));
+            Assert.Throws<KeyNotFoundException>(() => hashTable.Get(1));
+            Console.WriteLine("PASSED: RemoveTest");
+        }
+
+        public static void GetTest()
+        {
+            HashTable<int, string> hashTable = new HashTable<int, string>();
+            
+            hashTable.Add(1, "apple");
+            hashTable.Add(2, "banana");
+
+            string apple = hashTable.Get(1);
+            
+            Assert.AreEqual(apple, "apple");
+            Console.WriteLine("PASSED: GetTest");
+        }
+
+        public static void ContainsTest()
+        {
+            HashTable<int, string> hashTable = new HashTable<int, string>();
+            
+            hashTable.Add(1, "apple");
+            hashTable.Add(2, "banana");
+
+            Assert.IsTrue(hashTable.Contains("apple"));
+            Assert.IsFalse(hashTable.Contains("kivi"));
+            Console.WriteLine("PASSED: ContainsTest");
+        }
+
+        public static void ClearTest()
+        {
+            HashTable<int, string> hashTable = new HashTable<int, string>();
+            
+            hashTable.Add(1, "apple");
+            hashTable.Add(2, "banana");
+
+            hashTable.Clear();
+
+            Assert.AreEqual(0, hashTable.Size());
+            Console.WriteLine("PASSED: ClearTest");
+        }
+
+        public static void SizeTest()
+        {
+            HashTable<int, string> hashTable = new HashTable<int, string>();
+            
+            hashTable.Add(1, "apple");
+            hashTable.Add(2, "banana");
+
+            Assert.AreEqual(2, hashTable.Size());
+
+            hashTable.Add(5, "banana");
+
+            Assert.AreEqual(3, hashTable.Size());
+
+            Console.WriteLine("PASSED: SizeTest");
         }
     }   
 }
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 Object-oriented programming
 ========
 
-## Dequeue
+## Deque
 
 Write a generic data type for a deque and a randomized queue. The goal of this assignment is to implement elementary data structures using arrays and linked lists, and to introduce you to generics and iterators. Dequeue. A double-ended queue or deque (pronounced “deck”) is a generalization of a stack and a queue that supports adding and removing items from either the front or the back of the data structure. Create a generic data type Deque that implements the following API:
 
-{public class Deque : IIterator 
+public class Deque : IIterator {
 
 // construct an empty deque
 public Deque()
@@ -36,11 +36,11 @@ public static void main(String[] args)
 
 }
 
-{public interface IIterator<T>
+public interface IIterator<T>
 {
     bool HasNext { get; }
     T MoveNext();
-}}
+}
 
 > Performance requirements.
 >
@@ -50,7 +50,7 @@ public static void main(String[] args)
 
 A randomized queue is similar to a stack or queue, except that the item removed is chosen uniformly at random among items in the data structure. Create a generic data type RandomizedQueue that implements the following API:
 
-{public class RandomizedQueue<Item> implements Iterable<Item> 
+public class RandomizedQueue<Item> implements Iterable<Item> { 
 
     // construct an empty randomized queue
     public RandomizedQueue()
@@ -90,7 +90,7 @@ A randomized queue is similar to a stack or queue, except that the item removed 
 
 In class, the Set Abstract Data Type was covered and how to use Hash Tables to implement the Set ADT. One natural use of a set is to hold the words in a spelling dictionary. This makes looking up the words very efficient. In this assignment, you will implement and use such a dictionary. The interface of the Hashtable class is below:
 
-{public class HashTable<KItem, VItem>
+public class HashTable<KItem, VItem> {
      
         public HashTable()
         { ... }
