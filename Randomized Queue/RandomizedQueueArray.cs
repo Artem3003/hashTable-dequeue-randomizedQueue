0811fdf5785e79d17ab6210fc2c93db89f5d9849
Artem <temapro04@gmai.com>
2023-07-21 21:41:54 +0300

    Implementation of randomized queue array

--- Randomized Queue/RandomizedQueueArray.cs
+++ Randomized Queue/RandomizedQueueArray.cs
@@ -9,61 +9,91 @@ namespace HashTableDequeRandomizedQueue
         private Item[] randomizedQueue;
         private int size = 10;
         private int tail;
-
+        public Random random;
         // construct an empty randomized queue
         public RandomizedQueueArray()
         {
+            this.randomizedQueue = new Item[size];
+            this.tail = 0;
+            random = new Random();
         }
 
         // is the randomized queue empty?
         public bool IsEmpty()
         {
-            throw new ArgumentException();
+            return randomizedQueue.Length == 0;
         }
 
         // return the number of items on the randomized queue
         public int Size()
         {
-            throw new ArgumentException();
+            return tail;
         }
 
         // add the item
         public void Enqueue(Item item)
         {
-            throw new ArgumentException();
+            if (Size() > size)
+            {
+                Resize(Size() + 5);   
+            }
+            randomizedQueue[tail++] = item;
         }
 
         // remove and return a random item
         public Item Dequeue()
         {
-            throw new ArgumentException();
+            int index = random.Next(0, tail + 1);
+            Item deletedItem = randomizedQueue[index];
+
+            randomizedQueue = randomizedQueue.Except(new Item[] {deletedItem}).ToArray();
+            
+            tail--;
+            return deletedItem;
         }
 
         // return a random item (but do not remove it)
         public Item Sample()
         {
-            throw new ArgumentException();
+            int index = random.Next(0, tail);
+            return randomizedQueue[index];
         }
 
         private void Resize(int newSize)
         {
-            throw new ArgumentException();
+            Item[] newRandomizedQueue = new Item[newSize];
+
+            for (int i = 0; i <= randomizedQueue.Length; i++)
+            {
+                newRandomizedQueue[i] = randomizedQueue[i];                
+            }
+
+            randomizedQueue = newRandomizedQueue;
         }
 
         // return an independent iterator over items in random order
         public IEnumerator<Item> Iterator()
         {
-            throw new ArgumentException();
+            for (int i = 0; i < tail; i++)
+            {
+                yield return randomizedQueue[i];
+            }
         }
 
         public IEnumerator<Item> GetEnumerator()
         {
-            throw new ArgumentException();
+            for (int i = 0; i < tail; i++)
+            {
+                yield return randomizedQueue[i];
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new ArgumentException();
+            for (int i = 0; i < tail; i++)
+            {
+                yield return randomizedQueue[i];
+            }
         }
     }
 }
