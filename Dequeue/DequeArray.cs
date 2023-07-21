0b3c0088a24f7deef3f0c0442c828afffa4bb39c
Artem <temapro04@gmai.com>
2023-07-21 21:37:52 +0300

    Implemetation of deque array

--- Dequeue/DequeArray.cs
+++ Dequeue/DequeArray.cs
@@ -9,67 +9,117 @@ namespace HashTableDequeRandomizedQueue
         private int tail;
         private int size = 10;
 
-        public bool HasNext => throw new ArgumentException();
+        public bool HasNext => head <= tail;
         
         // construct a deque where user choose length
-
+        public DequeArray(int size)
+        {
+            this.deque = new Item[size];
+            this.head = size / 2;
+            this.tail = head;
+        }
         // construct an empty deque
         public DequeArray()
         {
-            
+            this.deque = new Item[size];
+            this.head = size / 2;
+            this.tail = head;
         }
 
         // is the deque empty?
         public bool IsEmpty()
         {
-            throw new ArgumentException();
+            return head >= tail;
         }
 
         // return the number of items on the deque
         public int Size()
         {
-            throw new ArgumentException();
+            if (tail >= size)
+            {
+                return tail + 1 - head;
+            }
+            return tail - head;
         }
 
         // add the item to the front
         public void AddFirst(Item item)
         {
-            throw new ArgumentException();
+            if (head == 0)
+            {
+                Resize(deque.Length * 2);
+            }
+            deque[head--] = item;
         }
 
         // add the item to the back
         public void AddLast(Item item)
         {
-            throw new ArgumentException();
+            if (tail == deque.Length - 1)
+            {
+                Resize(deque.Length * 2);
+            }
+            deque[++tail] = item;
         }
 
         // remove and return the item from the front
         public Item RemoveFirst()
         {
-            throw new ArgumentException();
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException();
+            }
+            Item item = deque[++head];
+            deque[head] = default(Item);
+            return item;
         }
 
         // remove and return the item from the back
         public Item RemoveLast()
         {
-            throw new ArgumentException();
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException();
+            }
+            Item item = deque[tail];
+            deque[tail--] = default(Item);
+            return item;
         }
 
         // resize deque
         private void Resize(int newSize)
         {
-            throw new ArgumentException();
+            Item[] newDeque = new Item[newSize];
+
+            int newHead = (newSize - Size()) / 2;
+            int newTail = newHead + Size() - 1;
+
+            for (int i = head, j = newHead; i <= tail; i++, j++)
+            {
+                newDeque[j] = deque[i];                
+            }
+
+            deque = newDeque;
+            head = newHead;
+            tail = newTail;
         }
 
         // return an iterator over items in order from front to back
         public IEnumerator<Item> Iterator()
         {
-            throw new ArgumentException();
+            for (int i = head; i <= tail; i++)
+            {
+                yield return deque[i];
+            }
         }
 
         public Item MoveNext()
         {
-            throw new ArgumentException();
+            if (HasNext)
+            {
+                return  deque[head++];
+            }
+            throw new InvalidOperationException();
         }
     }
 }
